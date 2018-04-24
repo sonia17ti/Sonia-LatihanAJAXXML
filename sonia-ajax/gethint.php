9e853507dce4f51ff973a73dfd7c8925f537ae98
sonia17ti <38453204+sonia17ti@users.noreply.github.com>
2018-04-24 15:10:22 +0700

    sonia

---
+++ sonia-ajax/gethint.php
@@ -0,0 +1,31 @@
+<?php
+// Array with names
+$a[] = "Sonia";
+$a[] = "Risa";
+$a[] = "Resti";
+$a[] = "Yuli";
+$a[] = "Ami";
+
+//get the q parameter from URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+//lookup all hints from array if $q is different from ""
+if ($q !== ""){
+	$q = strtolower($q);
+	$len = strlen($q);
+	foreach ($a as $name){
+		if (stristr($q, substr($name, 0, $len))){
+			if ($hint === ""){
+				$hint = $name;
+			} else {
+				$hint .= ", $name";
+			}
+		}
+	}
+}
+
+// Output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+?>
